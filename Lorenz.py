8c42c5a2d29f371a8b63b2dae577cf9050fce052
ellenyli <y.ellen.li@gmail.com>
2017-12-01 09:39:49 -0500

    More comparisons.

--- Lorenz.py
+++ Lorenz.py
@@ -42,202 +42,247 @@ import sparse_identification as sp
 
 def Lorenz(x0, sigma, rho, beta, time):
 
-    """
+	"""
 
-    This small function runs a simulation of the Lorenz system.
+	This small function runs a simulation of the Lorenz system.
 
-    Inputs
-    ------
+	Inputs
+	------
 
-    x0 : numpy array containing the initial condition.
+	x0 : numpy array containing the initial condition.
 
-    sigma, rho, beta : parameters of the Lorenz system.
+	sigma, rho, beta : parameters of the Lorenz system.
 
-    time : numpy array for the evaluation of the state of
-           the Lorenz system at some given time instants.
+	time : numpy array for the evaluation of the state of
+		   the Lorenz system at some given time instants.
 
-    Outputs
-    -------
+	Outputs
+	-------
 
-    x : numpy two-dimensional array.
-        State vector of the vector for the time instants
-        specified in time.
+	x : numpy two-dimensional array.
+		State vector of the vector for the time instants
+		specified in time.
 
-    xdot : corresponding derivatives evaluated using
-           central differences.
+	xdot : corresponding derivatives evaluated using
+		   central differences.
 
-    """
+	"""
 
-    def dynamical_system(y,t):
+	def dynamical_system(y,t):
 
-        dy = np.zeros_like(y)
-        dy[0] = sigma*(y[1]-y[0])
-        dy[1] = y[0]*(rho - y[2]) - y[1]
-        dy[2] = y[0]*y[1] - beta*y[2]
+		dy = np.zeros_like(y)
+		dy[0] = sigma*(y[1]-y[0])
+		dy[1] = y[0]*(rho - y[2]) - y[1]
+		dy[2] = y[0]*y[1] - beta*y[2]
 
-        return dy
+		return dy
 
-    x = odeint(dynamical_system,x0,time)
-    dt = time[1]-time[0]
-    from sparse_identification.utils import derivative
-    xdot = derivative(x,dt)
+	x = odeint(dynamical_system,x0,time)
+	dt = time[1]-time[0]
+	from sparse_identification.utils import derivative
+	xdot = derivative(x,dt)
 
-    return x, xdot
+	return x, xdot
 
 def constraints(library):
 
-    """
+	"""
 
-    This function illustrates how to impose some
-    user-defined constraints for the sparse identification.
+	This function illustrates how to impose some
+	user-defined constraints for the sparse identification.
 
-    Input
-    -----
+	Input
+	-----
 
-    library : library object used for the sparse identification.
+	library : library object used for the sparse identification.
 
-    Outputs
-    -------
+	Outputs
+	-------
 
-    C : two-dimensional numpy array.
-        Constraints to be imposed on the regression coefficients.
+	C : two-dimensional numpy array.
+		Constraints to be imposed on the regression coefficients.
 
-    d : one-dimensional numpy array.
-        Value of the constraints.
+	d : one-dimensional numpy array.
+		Value of the constraints.
 
-    """
+	"""
 
-    #--> Recover the number of input and output features of the library.
-    m = library.n_input_features_
-    n = library.n_output_features_
+	#--> Recover the number of input and output features of the library.
+	m = library.n_input_features_
+	n = library.n_output_features_
 
-    #--> Initialise the user-defined constraints matrix and vector.
-    #    In this example, two different constraints are imposed.
-    C = np.zeros((2, m*n))
-    d = np.zeros((2,1))
+	#--> Initialise the user-defined constraints matrix and vector.
+	#    In this example, two different constraints are imposed.
+	C = np.zeros((2, m*n))
+	d = np.zeros((2,1))
 
-    #--> Definition of the first constraint:
-    #    In the x-equation, one imposes that xi[2] = -xi[1]
-    #    Note: xi[0] corresponds to the bias, xi[1] to the coefficient
-    #    for x(t) and xi[2] to the one for y(t).
-    C[0, 1] = 1
-    C[0, 2] = 1
+	#--> Definition of the first constraint:
+	#    In the x-equation, one imposes that xi[2] = -xi[1]
+	#    Note: xi[0] corresponds to the bias, xi[1] to the coefficient
+	#    for x(t) and xi[2] to the one for y(t).
+	C[0, 1] = 1
+	C[0, 2] = 1
 
-    #--> Definition of the second constraint:
-    #    In the y-equation, one imposes that xi[1] = 28
-    #    Note: the n+ is because the coefficient xi[1] for
-    #    the y-equation is the n+1th entry of the regression
-    #    coefficients vector.
-    C[1, n+1] = 1
-    d[1] = 28
+	#--> Definition of the second constraint:
+	#    In the y-equation, one imposes that xi[1] = 28
+	#    Note: the n+ is because the coefficient xi[1] for
+	#    the y-equation is the n+1th entry of the regression
+	#    coefficients vector.
+	C[1, n+1] = 1
+	d[1] = 28
 
-    return C, d
+	return C, d
 
 def Identified_Model(y, t, library, estimator) :
 
-    '''
-    Simulates the model from Sparse identification.
+	'''
+	Simulates the model from Sparse identification.
 
-    Inputs
-    ------
+	Inputs
+	------
 
-    library: library object used in the sparse identification
-             (e.g. poly_lib = PolynomialFeatures(degree=3) )
+	library: library object used in the sparse identification
+			 (e.g. poly_lib = PolynomialFeatures(degree=3) )
 
-    estimator: estimator object obtained from the sparse identification
+	estimator: estimator object obtained from the sparse identification
 
-    Output
-    ------
+	Output
+	------
 
-    dy : numpy array object containing the derivatives evaluated using the
-         model identified from sparse regression.
+	dy : numpy array object containing the derivatives evaluated using the
+		 model identified from sparse regression.
 
-    '''
+	'''
 
-    dy = np.zeros_like(y)
+	dy = np.zeros_like(y)
 
-    lib = library.fit_transform(y.reshape(1,-1))
-    Theta = block_diag(lib, lib, lib)
-    dy = Theta.dot(estimator.coef_)
+	lib = library.fit_transform(y.reshape(1,-1))
+	Theta = block_diag(lib, lib, lib)
+	dy = Theta.dot(estimator.coef_)
 
-    return dy
+	return dy
 
 def plot_results(t, X, Y):
 
-    """
+	"""
 
-    Function to plot the results. No need to comment.
+	Function to plot the results. No need to comment.
 
-    """
+	"""
 
-    fig, ax = plt.subplots( 3 , 1 , sharex = True, figsize=(10,5) )
+	fig, ax = plt.subplots( 3 , 1 , sharex = True, figsize=(10,5) )
 
-    ax[0].plot(t  , X[:,0], label='Full simulation' )
-    ax[0].plot(t , Y[:,0], linestyle='--', label='Identified model')
-    ax[0].set_ylabel('x(t)')
-    ax[0].legend(loc='upper center', bbox_to_anchor=(.5, 1.33), ncol=2, frameon=False )
+	ax[0].plot(t  , X[:,0], label='Full simulation' )
+	ax[0].plot(t , Y[:,0], linestyle='--', label='Identified model')
+	ax[0].set_ylabel('x(t)')
+	ax[0].legend(loc='upper center', bbox_to_anchor=(.5, 1.33), ncol=2, frameon=False )
 
-    ax[1].plot(t, X[:,1])
-    ax[1].plot(t ,Y[:,1], ls='--')
-    ax[1].set_ylabel('y(t)')
+	ax[1].plot(t, X[:,1])
+	ax[1].plot(t ,Y[:,1], ls='--')
+	ax[1].set_ylabel('y(t)')
 
-    ax[2].plot(t, X[:,2])
-    ax[2].plot(t ,Y[:,2], ls='--')
-    ax[2].set_ylabel('z(t)')
-    ax[2].set_xlabel('Time')
-    ax[2].set_xlim(0, 20)
+	ax[2].plot(t, X[:,2])
+	ax[2].plot(t ,Y[:,2], ls='--')
+	ax[2].set_ylabel('z(t)')
+	ax[2].set_xlabel('Time')
+	ax[2].set_xlim(0, 20)
 
-    fig = plt.figure(figsize=(5,5))
+	fig = plt.figure(figsize=(5,5))
 
-    ax = fig.gca(projection='3d')
-    ax.plot(X[-2500:,0], X[-2500:,1], X[-2500:,2])
-    ax.plot(Y[-2500:,0], Y[-2500:,1], Y[-2500:,2], ls='--')
-    ax.axis('equal')
+	ax = fig.gca(projection='3d')
+	ax.plot(X[-2500:,0], X[-2500:,1], X[-2500:,2])
+	ax.plot(Y[-2500:,0], Y[-2500:,1], Y[-2500:,2], ls='--')
+	ax.axis('equal')
 
-    return
+	return
 
 
+def plot_multi_results(t, X1, Y1, X2, Y2):
+	fig, ax = plt.subplots( 3 , 2 , sharex = True, figsize=(10,5) )
 
+	ax[0][0].plot(t  , X1[:,0], label='Full simulation 1' )
+	ax[0][0].plot(t , Y1[:,0], linestyle='--', label='Identified model 1')
+	ax[0][0].set_ylabel('x(t)')
+	ax[0][0].legend(loc='upper center', bbox_to_anchor=(.5, 1.33), ncol=2, frameon=False )
+
+	ax[1][0].plot(t, X1[:,1])
+	ax[1][0].plot(t ,Y1[:,1], ls='--')
+	ax[1][0].set_ylabel('y(t)')
+
+	ax[2][0].plot(t, X1[:,2])
+	ax[2][0].plot(t ,Y1[:,2], ls='--')
+	ax[2][0].set_ylabel('z(t)')
+	ax[2][0].set_xlabel('Time')
+	ax[2][0].set_xlim(0, 20)
+
+	ax[0][1].plot(t  , X2[:,0], label='Full simulation 2' )
+	ax[0][1].plot(t , Y2[:,0], linestyle='--', label='Identified model 2')
+	ax[0][1].set_ylabel('x(t)')
+	ax[0][1].legend(loc='upper center', bbox_to_anchor=(.5, 1.33), ncol=2, frameon=False )
+
+	ax[1][1].plot(t, X2[:,1])
+	ax[1][1].plot(t ,Y2[:,1], ls='--')
+	ax[1][1].set_ylabel('y(t)')
+
+	ax[2][1].plot(t, X2[:,2])
+	ax[2][1].plot(t, Y2[:,2], ls='--')
+	ax[2][1].set_ylabel('z(t)')
+	ax[2][1].set_xlabel('Time')
+	ax[2][1].set_xlim(0, 20)
+
+	fig = plt.figure(figsize=(10,5))
+
+	ax = fig.add_subplot(1, 2, 1, projection='3d')
+	ax.plot(X1[-2500:,0], X1[-2500:,1], X1[-2500:,2])
+	ax.plot(Y1[-2500:,0], Y1[-2500:,1], Y1[-2500:,2], ls='--')
+	ax.axis('equal')
+
+	ax = fig.add_subplot(1, 2, 2, projection='3d')
+	ax.plot(X2[-2500:,0], X2[-2500:,1], X2[-2500:,2])
+	ax.plot(Y2[-2500:,0], Y2[-2500:,1], Y2[-2500:,2], ls='--')
+	ax.axis('equal')
+
+	return
 
 
 if __name__ == '__main__':
 
-    #--> Sets the parameters for the Lorenz system.
-    sigma = 10.
-    rho = 28.
-    beta = 8./3.
+	#--> Sets the parameters for the Lorenz system.
+	sigma = 10.
+	rho = 28.
+	beta = 8./3.
 
-    t = np.linspace(0,100,10000)
+	t = np.linspace(0,100,10000)
 
-    #--> Run the Lorenz system to produce the data to be used in the sparse identification.
-    x0 = np.array([-8., 7., 27.])
-    X, dX = Lorenz(x0, sigma, rho, beta, t)
+	#--> Run the Lorenz system to produce the data to be used in the sparse identification.
+	x0 = np.array([-8., 7., 27.])
+	X, dX = Lorenz(x0, sigma, rho, beta, t)
 
-    #-----------------------------------------------------------------------
-    #-----                                                             -----
-    #-----     Sparse Identification of Nonlinear Dynamics (SINDY)     -----
-    #-----                                                             -----
-    #-----------------------------------------------------------------------
+	#-----------------------------------------------------------------------
+	#-----                                                             -----
+	#-----     Sparse Identification of Nonlinear Dynamics (SINDY)     -----
+	#-----                                                             -----
+	#-----------------------------------------------------------------------
 
-    # ---> Compute the Library of polynomial features.
-    poly_lib = PolynomialFeatures(degree=2, include_bias=True)
-    lib = poly_lib.fit_transform(X)
-    Theta = block_diag(lib, lib, lib)
-    n_lib = poly_lib.n_output_features_
+	# ---> Compute the Library of polynomial features.
+	poly_lib = PolynomialFeatures(degree=2, include_bias=True)
+	lib = poly_lib.fit_transform(X)
+	Theta = block_diag(lib, lib, lib)
+	n_lib = poly_lib.n_output_features_
 
-    b = dX.flatten(order='F')
-    A = Theta
+	b = dX.flatten(order='F')
+	A = Theta
 
-    # ---> Specify the user-defined constraints.
-    C, d = constraints(poly_lib)
+	# ---> Specify the user-defined constraints.
+	C, d = constraints(poly_lib)
 
-    # ---> Create a linear regression estimator using sindy and identify the underlying equations.
-    estimator = sp.sindy(l1=0.01, solver='lasso')
-    estimator.fit(A, b)#, eq=[C, d])
+	# ---> Create a linear regression estimator using sindy and identify the underlying equations.
+	estimator = sp.sindy(l1=0.01, solver='lasso')
+	estimator.fit(A, b)#, eq=[C, d])
 
-    #--> Simulates the identified model.
-    Y  = odeint(Identified_Model, x0, t, args=(poly_lib, estimator))
+	#--> Simulates the identified model.
+	Y  = odeint(Identified_Model, x0, t, args=(poly_lib, estimator))
 
-    #--> Plots the results to compare the dynamics of the identified system against the original one.
-    plot_results(t, X, Y)
-    plt.show()
+	#--> Plots the results to compare the dynamics of the identified system against the original one.
+	plot_results(t, X, Y)
+	plt.show()
--- Lorenz_tests.py
+++ Lorenz_tests.py
@@ -1,44 +1,36 @@
 from Lorenz import *
 import numpy as np
 
-def main(sigma, rho, beta, x0):
-	t = np.linspace(0,100,10000)
-
-	#--> Run the Lorenz system to produce the data to be used in the sparse identification.
-	X, dX = Lorenz(x0, sigma, rho, beta, t)
-
-	#-----------------------------------------------------------------------
-	#-----                                                             -----
-	#-----     Sparse Identification of Nonlinear Dynamics (SINDY)     -----
-	#-----                                                             -----
-	#-----------------------------------------------------------------------
-
-	# ---> Compute the Library of polynomial features.
+def simulate(t, x0, X, dX):
 	poly_lib = PolynomialFeatures(degree=2, include_bias=True)
 	lib = poly_lib.fit_transform(X)
 	Theta = block_diag(lib, lib, lib)
 	n_lib = poly_lib.n_output_features_
-
 	b = dX.flatten(order='F')
 	A = Theta
-
-	# ---> Specify the user-defined constraints.
 	C, d = constraints(poly_lib)
-
-	# ---> Create a linear regression estimator using sindy and identify the underlying equations.
 	estimator = sp.sindy(l1=0.01, solver='lasso')
-	estimator.fit(A, b)#, eq=[C, d])
-
-	#--> Simulates the identified model.
+	estimator.fit(A, b)
 	Y  = odeint(Identified_Model, x0, t, args=(poly_lib, estimator))
 
+	return Y
+
+def main(vars1, vars2):
+	(sigma1, rho1, beta1, x0) = vars1; (sigma2, rho2, beta2, xx0) = vars2 
+
+	t = np.linspace(0,100,10000)
+	X1, dX1 = Lorenz(x0, sigma1, rho1, beta1, t)
+	Y1 = simulate(t, x0, X1, dX1)
+	X2, dX2 = Lorenz(xx0, sigma2, rho2, beta2, t)
+	Y2 = simulate(t, xx0, X2, dX2)
+
 	#--> Plots the results to compare the dynamics of the identified system against the original one.
-	plot_results(t, X, Y)
+	plot_multi_results(t, X1, Y1, X2, Y2)
 	plt.show()
 
-main(10., 28., 8./3., np.array([-8., 7., 27.]))
-main(10.1, 28., 8./3., np.array([-8., 7., 27.]))
-main(10.5, 28., 8./3., np.array([-8., 7., 27.]))
-main(10., 28., 8./3., np.array([-8.1, 7., 27.]))
-main(10., 28., 8./3., np.array([-8., 7.1, 27.]))
-main(10., 28., 8./3., np.array([-8., 7., 27.1]))
+main((10., 28., 8./3., np.array([-8., 7., 27.])), (10.1, 28., 8./3., np.array([-8., 7., 27.])))
+main((10., 28., 8./3., np.array([-8., 7., 27.])), (10, 28.1, 8./3., np.array([-8., 7., 27.])))
+main((10., 28., 8./3., np.array([-8., 7., 27.])), (10.1, 28., 8.5/3., np.array([-8., 7., 27.])))
+main((10., 28., 8./3., np.array([-8., 7., 27.])), (10., 28., 8./3., np.array([-8.1, 7., 27.])))
+main((10., 28., 8./3., np.array([-8., 7., 27.])), (10., 28., 8./3., np.array([-8., 7.1, 27.])))
+main((10., 28., 8./3., np.array([-8., 7., 27.])), (10., 28., 8./3., np.array([-8., 7., 27.1])))
