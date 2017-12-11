1332ae20c9fd5b8f3aeb4ae77b6e72b0c0c7e417
Alexander Whatley <alexanderwhatley@college.harvard.edu>
2017-12-11 02:18:40 -0500

    Added some new figures for 3 dimensional system.

--- Lotka_Volterra.py
+++ Lotka_Volterra.py
@@ -6,7 +6,7 @@ from scipy.linalg import block_diag
 from sklearn.preprocessing import PolynomialFeatures
 import sparse_identification as sp
 
-def Lotka_Volterra(x0, alpha, beta, time):
+def Lotka_Volterra(x0, alpha, beta, time, noise=0):
 
     """
 
@@ -41,8 +41,38 @@ def Lotka_Volterra(x0, alpha, beta, time):
         return dy
 
     x = odeint(dynamical_system,x0,time)
+    x += np.random.normal(0, noise, x.shape)
     dt = time[1]-time[0]
     from sparse_identification.utils import derivative
     xdot = derivative(x,dt)
     
     return x, xdot
+
+def Identified_Model(y, t, library, estimator) :
+
+    '''
+    Simulates the model from Sparse identification.
+
+    Inputs
+    ------
+
+    library: library object used in the sparse identification
+             (e.g. poly_lib = PolynomialFeatures(degree=3) )
+
+    estimator: estimator object obtained from the sparse identification
+
+    Output
+    ------
+
+    dy : numpy array object containing the derivatives evaluated using the
+         model identified from sparse regression.
+
+    '''
+
+    dy = np.zeros_like(y)
+
+    lib = library.fit_transform(y.reshape(1,-1))
+    Theta = block_diag(lib, lib, lib)
+    dy = Theta.dot(estimator.coef_)
+
+    return dy
